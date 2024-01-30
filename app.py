4f06a03a48475c668763754580d5412a3a559da7
ribenaa <144659319+ribenaa@users.noreply.github.com>
2024-01-30 20:37:11 +0800

    ToDo app

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# set up application
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+# index route
+# set up route with app route decorator
+# pass in url string of route ('/')
+
+
+@app.route('/', methods=['POST', 'GET'])
+# define function for that route
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = ToDo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = ToDo.query.order_by(ToDo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delte(id):
+    task_to_delete = ToDo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = ToDo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was a problem updating the task'
+
+    else:
+        return render_template('update.html', task=task)
+
+
+# if there are any errors, show on webpage
+if __name__ == "__main__":
+    # Create the application context
+    with app.app_context():
+        # Create the database tables
+        db.create_all()
+
+    # Run the development server
+    app.run(debug=True)
